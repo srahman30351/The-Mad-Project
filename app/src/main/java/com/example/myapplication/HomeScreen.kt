13292f018e19d524998b3e3d7a2bfb2a17217dc1
srahman30351 <safwanrahman107@gmail.com>
2025-02-21 13:45:54 +0000

    Added starter navigation for the screen home, user and settings

---
+++ app/src/main/java/com/example/myapplication/HomeScreen.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(navController: NavController){
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Home")
+        Button(onClick = {
+            navController.navigate("user_screen")
+        }) {
+            Text(text = "Go to users")
+        }
+    }
+}
